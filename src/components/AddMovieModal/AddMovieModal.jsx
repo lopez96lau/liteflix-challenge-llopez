21f618abfa3236bd3ca982f46f12b92cebd059b2
Laureano López <lopez96lau@gmail.com>
2022-01-08 14:59:23 -0300

    Componentes base y normalización CSS

---
+++ src/components/AddMovieModal/AddMovieModal.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './AddMovieModal.module.css';
+
+const AddMovieModal = () => {
+  return (
+    <div className={styles.addMovieModal}>
+      AddMovieModal
+    </div>
+  );
+}
+
+export default AddMovieModal;
